6212221cddbdfd3637f320db62876e8709a2d738
Andrew Fung <andrew.fungkinho@gmail.com>
2020-09-26 23:25:44 +0800

    ncr for social distance

--- codeitsuisse/routes/socialdistance.py
+++ codeitsuisse/routes/socialdistance.py
@@ -7,23 +7,30 @@ from codeitsuisse import app
 
 logger = logging.getLogger(__name__)
 
-def ways_of_sitting(seats, people, space):
-  if seats <= 0:
-      return 0
-  elif people == 1:
-      # One person left. The person can sit on any seats.
-      return seats
-  elif people + space * (people-1) > seats:
-      # Not enough space
-      return 0
+def nCr(n, r):
+    result = 1
+    # nPr = n! / (n-r)!
+    for i in range(n, n-r, -1):
+        result *= i
+    # nCr = nPr / r!
+    for i in range(1, r+1):
+        result //= i
+    return result
 
-  count = 0
-  for i in range(seats):
-      # -1 since this passenger already took a seat
-      # -space since the the next passenger must maintain social distance
-      # -i is to iterate through the possible seats
-      count += ways_of_sitting(seats - 1 - space - i, people - 1, space)
-  return count
+def ways_of_sitting(seats, people, space):
+    # Minimum seats: PSPSP...
+    min_seats = people + space * (people-1)
+    if seats >= min_seats:
+        # How many slots we can stuff empty seats in (value of n)
+        # One slot is before the first person (on the left)
+        # For every person we add, we add a slot (on the right of the person)
+        slots = 1 + people
+        # How many we need to choose (value of r)
+        choose = seats - min_seats
+        # Number of combinations with replacement is nCr(n+r-1, r)
+        return nCr(slots + choose - 1, choose)
+    else:
+        return 0
 
 @app.route('/social_distancing', methods=['POST'])
 def social_distancing():
@@ -34,6 +41,3 @@ def social_distancing():
         answers[key] = ways_of_sitting(value['seats'],value['people'],value['spaces'])
     solution = {'answers': answers}
     return jsonify(solution)
-
-
-
