3e8896522a8e1c10adfdb8c07b83dd7d32984e4c
Andrew Fung <andrew.fungkinho@gmail.com>
2020-09-26 21:29:01 +0800

    disabled functions

--- codeitsuisse/routes/square.py
+++ codeitsuisse/routes/square.py
@@ -7,21 +7,21 @@ from codeitsuisse import app
 
 logger = logging.getLogger(__name__)
 
-@app.route('/square', methods=['POST'])
-def evaluate():
-    data = request.get_json()
-    logging.info("data sent for evaluation {}".format(data))
-    # logging.info(f"request {request}")
+# @app.route('/square', methods=['POST'])
+# def evaluate():
+#     data = request.get_json()
+#     logging.info("data sent for evaluation {}".format(data))
+#     # logging.info(f"request {request}")
 
-    name = data['name']
-    location = data['location']
+#     name = data['name']
+#     location = data['location']
 
-    randomlist = data['randomlist']
+#     randomlist = data['randomlist']
 
 
-    # logging.info("My result :{}".format(result))
-    return jsonify({'result': 'Success!', 'name': name, 'location': location, 'randomkeyinlist': randomlist[1]})
-    # json.dumps(result)
+#     # logging.info("My result :{}".format(result))
+#     return jsonify({'result': 'Success!', 'name': name, 'location': location, 'randomkeyinlist': randomlist[1]})
+#     # json.dumps(result)
 
 
 
