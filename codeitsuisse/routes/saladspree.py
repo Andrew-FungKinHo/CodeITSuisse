c66dcde681580bd5bfd22c7e612b5656c8e36b2d
Andrew Fung <andrew.fungkinho@gmail.com>
2020-09-27 00:33:30 +0800

    saladspree

--- codeitsuisse/routes/saladspree.py
+++ codeitsuisse/routes/saladspree.py
@@ -1,20 +1,36 @@
 import logging
 import json
-
+import math
 from flask import request, jsonify
 
 from codeitsuisse import app
 
 logger = logging.getLogger(__name__)
 
+def getItemSmallest(item,number_of_salads):
+  itemSmallest = math.inf
+  startingPoint = 0
+  for i in range(len(item) - number_of_salads + 1):
+    sublist = item[startingPoint:startingPoint + number_of_salads]
+    if 'X' not in sublist:
+      sublist = list(map(int, sublist)) 
+      if sum(sublist) < itemSmallest and sum(sublist)>0:
+        itemSmallest = sum(sublist)
+      else:
+        continue
+    startingPoint += 1
+
+  return itemSmallest
+
 @app.route('/salad-spree', methods=['POST'])
 def saladSpree():
     data = request.get_json()
-    logging.info("data sent for evaluation {}".format(data))
-    inputValue = data.get("number_of_salads")
-    result = inputValue * inputValue * inputValue * inputValue
-    logging.info("My result :{}".format(result))
-    return json.dumps(result)
+    listSmallest = math.inf
+    for j in range(len(data['salad_prices_street_map'])):
+        smallest = getItemSmallest(data['salad_prices_street_map'][j],data['number_of_salads'])
+        if smallest < listSmallest and smallest > 0:
+            listSmallest = smallest
+    return json.dumps(listSmallest)
 
 
 
